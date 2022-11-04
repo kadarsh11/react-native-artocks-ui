3531a7c10a6370f721a1e277ca904b8e56db456a
Adarsh Kumar <kadarsh.singh99@gmail.com>
2022-11-04 14:15:46 +0530

    trying memo

--- src/components/view/index.tsx
+++ src/components/view/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  InteractionManager,
   StyleSheet,
   TouchableOpacity,
   View as RNView,
@@ -23,48 +24,55 @@ interface Props {
   children: React.ReactNode;
 }
 
-export const View: React.FC<Props> = ({
-  style = {},
-  m = 0,
-  p = 0,
-  bg = '',
-  flex,
-  size = 0,
-  radius = 0,
-  row = false,
-  center = false,
-  verticalCenter = false,
-  horizontalCenter = false,
-  children,
-  onPress,
-}) => {
-  const st = Array.isArray(style) ? style : [style];
-  const compStyle = [
-    flex ? { flex } : {},
-    convertMargin(m),
-    convertPadding(p),
-    bg ? { backgroundColor: bg } : {},
-    row ? styles.row : {},
-    radius ? { borderRadius: radius } : {},
-    size ? { height: size, width: size } : {},
-    (verticalCenter && row) || (horizontalCenter && !row)
-      ? styles.alignCenter
-      : {},
-    (verticalCenter && !row) || (horizontalCenter && row)
-      ? styles.justifyCenter
-      : {},
-    center ? styles.center : {},
-    ...st,
-  ];
-  if (onPress) {
-    return (
-      <TouchableOpacity style={compStyle} onPress={onPress}>
-        {children}
-      </TouchableOpacity>
-    );
+export const View: React.FC<Props> = React.memo(
+  ({
+    style = {},
+    m,
+    p,
+    bg = '',
+    flex,
+    size = 0,
+    radius = 0,
+    row = false,
+    center = false,
+    verticalCenter = false,
+    horizontalCenter = false,
+    children,
+    onPress,
+  }) => {
+    const st = Array.isArray(style) ? style : [style];
+    const compStyle = [
+      flex ? { flex } : {},
+      m && convertMargin(m),
+      p && convertPadding(p),
+      bg ? { backgroundColor: bg } : {},
+      row ? styles.row : {},
+      radius ? { borderRadius: radius } : {},
+      size ? { height: size, width: size } : {},
+      (verticalCenter && row) || (horizontalCenter && !row)
+        ? styles.alignCenter
+        : {},
+      (verticalCenter && !row) || (horizontalCenter && row)
+        ? styles.justifyCenter
+        : {},
+      center ? styles.center : {},
+      ...st,
+    ];
+    if (onPress) {
+      return (
+        <TouchableOpacity
+          style={compStyle}
+          onPress={() => {
+            InteractionManager.runAfterInteractions(onPress);
+          }}
+        >
+          {children}
+        </TouchableOpacity>
+      );
+    }
+    return <RNView style={compStyle}>{children}</RNView>;
   }
-  return <RNView style={compStyle}>{children}</RNView>;
-};
+);
 
 const styles = StyleSheet.create({
   row: {
